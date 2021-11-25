44417c715bb492022ee1c2dbe2233c728c0be656
Suraj Narsale <surajsnarsale@gmail.com>
2021-11-25 21:42:03 +0530

    feat: add head component for better seo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { About, Footer, Hero, Navbar, Projects } from '../components';
+import React from "react";
+import Head from "next/head";
+import { About, Footer, Hero, Navbar, Projects } from "../components";
 
 export default function index() {
 	return (
 		<>
+			<Head>
+				<title>surajnarsale portfolio site</title>
+				<meta
+					name="description"
+					content="If you need to know one thing about Suraj, it's this: Design + Code =⚡️"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<meta charset="UTF-8" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Head>
 			<Navbar />
 			<Hero />
 			<Projects />
